4040c5f53edf62c90aaa6346e992dcd94104df7b
Bhavesh Praveen <bhaveshpraveen10@gmail.com>
2018-03-21 03:51:45 +0530

    Fix Postgres issue. Add Psycopg2 to requirements. Update settings

--- bitchat/settings.py
+++ bitchat/settings.py
@@ -53,7 +53,8 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
+            # my ip
+            "hosts": [('172.23.0.1', 6379)],
         },
     },
 }
@@ -94,12 +95,24 @@ WSGI_APPLICATION = 'bitchat.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
+
+
 
 
 # Password validation
