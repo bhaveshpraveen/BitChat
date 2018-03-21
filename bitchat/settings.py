4e0fbf8fb67591d8f765268cfc66bc06db25eedf
Yaswant Narayan <yoyaswant@gmail.com>
2018-03-21 10:06:08 +0530

    change settings.py

--- bitchat/settings.py
+++ bitchat/settings.py
@@ -94,10 +94,20 @@ WSGI_APPLICATION = 'bitchat.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
